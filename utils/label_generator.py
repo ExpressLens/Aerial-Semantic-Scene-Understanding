28e45c62f66f9f9d22ca1898957d384c69398899
ExpressLens <expresslens@proton.me>
2023-04-30 23:05:37 +0000

    Update Chapter7/Ex1.md

--- utils/label_generator.py
+++ utils/label_generator.py
@@ -8,3 +8,18 @@ import glob
 import os
 import os.path as osp
 import sys
+
+import imgviz
+import numpy as np
+
+import labelme
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('input_dir', help='input annotated directory')
+    parser.add_argument('output_dir', help='output dataset directory')
+    parser.add_argument('--labels', help='labels file', required=True)
+    parser.add_argument(
